e3d2ef9db972ca820fdb92dfbf1c35d1833d81e4
Alex Newbegin <47902821+anewbegin95@users.noreply.github.com>
2021-08-11 20:38:58 -0400

    Add files via upload
    Added cleansing, calculated new fields, added charting

--- build/wp001-sox_proj_wpct.py
+++ build/wp001-sox_proj_wpct.py
@@ -11,6 +11,7 @@ pythagorean expectation and game-by-game results as of 7:00 PM on Monday, 8/9
 
 # %% 1. Import modules
 import pandas as pd
+import matplotlib.pyplot as plt
 
 # %% 2. Import data
 data = pd.read_csv(r'/Users/YouCanCallMeAll/Desktop/Baseball/wp001-sox_proj_wpct/data/sox_game_results.csv')
@@ -18,17 +19,48 @@ print(data.head())
 for col in data.columns:
     print(col)
 
-# %% Cleanse data
-    
-#rename columns
-data.rename(columns={"Unnamed: 2": "gp_indc"})
-    
-# %% Calculate total runs, runs allowed, and games played YTD
-
-#filter for only data from games played
+# %% Create filtered dataset to only include data from played games
 gp_data = data[data['gp_indc'] == 'boxscore']
 #print(gp_data)
 
+# %% Cleanse data
+    
+#rename columns
+gp_data = gp_data.rename(columns={"Unnamed: 2": "gp_indc"})
+
+#change data types
+gp_data['m#'] = gp_data['m#'].astype(int)
+gp_data['R'] = gp_data['R'].astype(int)
+gp_data['RA'] = gp_data['RA'].astype(int)
+
+#print(gp_data.head())
+#for col in gp_data.columns:
+#    print(col)
+
+# %% Create cumulative sums of runs scored and runs allowed
+gp_data['cumsum_rs'] = gp_data['R'].cumsum()
+gp_data['cumsum_ra'] = gp_data['RA'].cumsum()
+
+print(gp_data.head())
+for col in gp_data.columns:
+    print(col)
+
+# %% Plot cumulative runs scored and cumulative runs allowed across games played
+plt.plot(gp_data['m#'], gp_data['cumsum_rs'], color='#0C2340')
+plt.plot(gp_data['m#'], gp_data['cumsum_ra'], color='#BD3039')
+plt.legend(['Runs Scored', 'Runs Allowed'])
+plt.show()
+
+# %% Plot cumulative vs scored and cumulative runs allowed across games played
+plt.plot(gp_data['cumsum_ra'], gp_data['cumsum_rs'], color='#0C2340')
+plt.show()
+
+# %% Plot runs scored versus runs allowed across games played
+plt.scatter(gp_data['RA'], gp_data['R'], color='#0C2340')
+plt.show()
+
+# %% Calculate total runs, runs allowed, and games played YTD
+    
 #calculate runs scored and save to variable 'rs'
 rs = pd.to_numeric(gp_data['R']).sum()
 #print(rs)
@@ -39,10 +71,6 @@ ra = pd.to_numeric(gp_data['RA']).sum()
 
 gp = gp_data['gp_indc'].count()
 #print(gp)
-
-# %% Create cumulative sums of runs scored and runs allowed
-
-# %% Plot runs scored and runs allowed across games played
 
 # %% Calculate current pythagorean win expectation
 exp = 1.85
